3f5752b057acf8f36f2cc801b8241da8e3f45537
chen <wszgrcy@gmail.com>
2026-05-27 18:35:06 +0800

    类型

--- packages/workflow/share/util/preset/textarea-template.ts
+++ packages/workflow/share/util/preset/textarea-template.ts
@@ -1,8 +1,24 @@
-import { actions, setComponent } from '@piying/view-angular-core';
+import { actions, asControl, setComponent } from '@piying/view-angular-core';
 import * as v from 'valibot';
-import type { SimplifiedState } from '@shenghuabi/lexical-textarea';
 export const TextareaTemplateDefine = v.pipe(
-  v.custom<SimplifiedState>((input) => !!input && Array.isArray(input)),
+  v.array(
+    v.array(
+      v.union([
+        v.object({ text: v.string(), type: v.literal('text') }),
+        v.object({
+          item: v.object({
+            label: v.string(),
+            value: v.array(v.union([v.string(), v.number()])),
+            type: v.optional(v.picklist(['custom'])),
+            suffix: v.optional(v.string()),
+          }),
+          type: v.literal('variable'),
+        }),
+      ]),
+    ),
+  ),
+  asControl(),
+  // v.custom<SimplifiedState>((input) => !!input && Array.isArray(input)),
   setComponent('textarea-template'),
   actions.class.top('nodrag'),
 );
