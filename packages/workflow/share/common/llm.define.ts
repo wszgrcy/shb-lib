15438f95eb8fe9ae722d3aba733df521a15792d1
chen <wszgrcy@gmail.com>
2026-06-10 22:41:33 +0800

    build

--- packages/workflow/share/common/llm.define.ts
+++ packages/workflow/share/common/llm.define.ts
@@ -1,9 +1,12 @@
-
-
 import * as v from 'valibot';
 import { ModelConfigDefine } from '@shenghuabi/openai';
+import { setComponent } from '@piying/view-angular-core';
 export function llmModelConfig(item?: { label: string }) {
-  return v.pipe(ModelConfigDefine, v.title(item?.label ?? '对话模型'));
+  return v.pipe(
+    ModelConfigDefine,
+    v.title(item?.label ?? '对话模型'),
+    setComponent('reset-card'),
+  );
 }
 
 export type ModelInputConfig = v.InferOutput<ReturnType<typeof llmModelConfig>>;
